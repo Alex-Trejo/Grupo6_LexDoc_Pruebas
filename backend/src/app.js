db0edacd5d5144f66cea82308fb85e31e45499a0
Alex <lanchado10@gmail.com>
2025-06-03 10:57:48 -0500

    actualizacion de rutas permitidas

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,6 +19,8 @@ const app = express();
 // Middleware CORS seguro
 const allowedOrigins = [
   'http://localhost:3000', 
+  'http://localhost:5173',
+  'http://localhost:5174'
 ];
 
 const corsOptions = {
