634346edb9fe024e20e71579237e1ce2510606ca
Alex <lanchado10@gmail.com>
2025-06-03 09:27:02 -0500

    Arreglo de backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,8 @@
 // server.js
-import app from "./src/app.js";
+import app from "./app.js";
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log("Server running");
+  console.log("Server running en http://localhost:" + (process.env.PORT || 3000));
 });
 
 export default server;
