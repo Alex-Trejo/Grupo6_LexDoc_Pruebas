8ca3bb48b3402567e1be31ab8326b78293cd4268
Bryan406 <rquisper406@gmail.com>
2025-06-03 10:47:59 -0500

    Register devuelto

--- frontend/src/api/authApi.jsx
+++ frontend/src/api/authApi.jsx
@@ -1,5 +1,5 @@
 export async function loginUser(credentials) {
-  const response = await fetch('http://localhost:3000/api/accounts/login', {
+  const response = await fetch('http://localhost:3000/api', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(credentials),
