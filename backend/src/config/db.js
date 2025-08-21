aae3982a3667415e22251c210444e978a086dfce
Alex <lanchado10@gmail.com>
2025-08-21 00:26:37 -0500

    config de deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,12 +4,21 @@ dotenv.config();
 
 const { Pool } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT, 10),
-  database: process.env.DB_NAME,
-});
+const connectionConfig = process.env.DATABASE_URL ? 
+  {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false // Requerido para conexiones a Render
+    }
+  } : 
+  {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,  
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT, 10),
+    database: process.env.DB_NAME,
+  };
+
+const pool = new Pool(connectionConfig);
 
 export default pool;
